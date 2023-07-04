e09145d9ee6be57d7c96ebe5ae410c8ea7906b84
Omkar Deshmukh <132034602+omkardwork603@users.noreply.github.com>
2023-07-04 19:56:04 +0530

    Add files via upload

---
+++ login_form.php
@@ -0,0 +1,64 @@
+
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(isset($_POST['submit'])){
+    
+   $email = mysqli_real_escape_string($conn, $_POST['usermail']);
+   $pass = md5($_POST['password']);
+
+   $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass'";
+
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+      $_SESSION['usermail'] = $email;
+      header('location:header.php');
+   }else{
+      $error[] = 'incorrect password or email.';
+   }
+
+}
+
+?>
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<div class="form-container">
+
+    <form action="" method="post">
+        <h3 class="title">login now</h3>
+        <?php
+         if(isset($error)){
+            foreach($error as $error){
+               echo '<span class="error-msg">'.$error.'</span>';
+            }
+         }
+      ?>
+        <input type="email" name="usermail" placeholder="enter your email" class="box" required>
+        <input type="password" name="password" placeholder="enter your password" class="box" required>
+        <input type="submit" value="login now" class="form-btn" name="submit">
+        <p>don't have an account? <a href="register_form.php">register now!</a></p>
+    </form>
+
+</div>
+</body>
+</html>
---
+++ user_db.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 04, 2023 at 04:25 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `user_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_form`
+--
+
+CREATE TABLE `user_form` (
+  `id` int(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_form`
+--
+
+INSERT INTO `user_form` (`id`, `email`, `password`) VALUES
+(1, 'vishal@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b'),
+(2, 'manish@gmail.com', 'e10adc3949ba59abbe56e057f20f883e'),
+(3, 'mohana@gmail.com', 'e9206237def4b4ef46fd933ed0f5a08f'),
+(4, 'vishale@gmail.com', 'e10adc3949ba59abbe56e057f20f883e');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_form`
+--
+ALTER TABLE `user_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_form`
+--
+ALTER TABLE `user_form`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
